e1a0dae14d9b819295a1a74c261685dce46ca703
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-21 23:47:32 +0530

    feat: load database URL from environment variables using dotenvy

--- crates/voxora-db/src/pool.rs
+++ crates/voxora-db/src/pool.rs
@@ -1,16 +1,19 @@
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::time::Duration;
+use dotenvy::dotenv;
 
 pub async fn create_pool_connection() -> Result<PgPool, sqlx::Error> {
+    dotenv().ok();
 
-
+    //extract the database url from the env file
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .min_connections(1)
         .acquire_timeout(Duration::from_secs(5))
-        .connect("postgres://user:password@localhost:5432/database")
+        .connect(&database_url)
         .await?;
 
     Ok(pool)
