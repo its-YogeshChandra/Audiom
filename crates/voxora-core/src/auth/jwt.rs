bf37c98bc277db579ce5cf6a1bb5f337632f017e
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-22 18:42:56 +0530

    refactor: remove unused JWT token creation logic from auth module

--- crates/voxora-core/src/auth/jwt.rs
+++ crates/voxora-core/src/auth/jwt.rs
@@ -14,29 +14,6 @@ pub struct Claims{
     name: String,
 }
 
-pub fn create_token (data: Claims) -> String {
-dotenv().ok();
-
-let jwt_secret_key = env::var("JWT_SECRET").expect("JWT_SECRET not found");
-let encoding_key = EncodingKey::from_secret(jwt_secret_key.as_bytes());
-
-//create the header
-let header = Header::default();
-
-//encode the given data 
-let token = match encode(&header, &data, &encoding_key){
-    Ok(token) => token,
-    Err(error) => {
-        eprintln!("Error creating token: {}", error);
-        return error.to_string()
-    },
-};
-
-//for check we can decode and check if we get the same values or not 
-//but that's future me problem 
- token
-}
-
 //verify the jwt token
 pub fn verify_token(token: &str) -> Result<Claims, String> {
     dotenv().ok();
