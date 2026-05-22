1cbfc547fb2fd5dae6d1b367bbdb96df9adaff42
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-22 19:30:47 +0530

    feat: implement JWT token verification and user database lookup in signup controller

--- crates/voxora-api/src/controllers/auth.rs
+++ crates/voxora-api/src/controllers/auth.rs
@@ -1,14 +1,31 @@
 use actix_web::{post, web, HttpResponse};
+use voxora_core::verify_token;
+use voxora_db::get_user_data;
 
 //signup endpoint 
 #[post("/signup")]
-pub async fn signup() -> Result<HttpResponse, actix_web::Error> {
+pub async fn signup(jwt_token: String) -> Result<HttpResponse, actix_web::Error> {
     //take the token from the frontend(generated through clerk) and verify it 
+    let result = match verify_token(&jwt_token) {
+    Ok(claims) => {
+        tracing::info!("Token verified successfully: {:#?}", claims);
+        claims
+    }
+    Err(error) => {
+        //handling should be done here I guess 
+        tracing::error!("Failed to verify token: {}", error);
+    }
+    };
+
+    //call the database function to check for the user info we get 
+    let db_result = get_user_data(pool, user_data)
+    
+
 
     //check for the credentials in the database 
         //if present then send error 
         //if not then call the create user function of the database 
-        
+
     Ok(HttpResponse::Ok().body("signup"))
 }
 
--- crates/voxora-core/src/lib.rs
+++ crates/voxora-core/src/lib.rs
@@ -1,4 +1,6 @@
 mod auth;
 mod models;
 
-//
+//extend all the services
+pub use auth::*;
+pub use models::*;
