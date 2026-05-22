f277c5053a0de851873cd3f0eeb15f4e59452183
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-22 23:54:18 +0530

    feat: initialize and inject database connection pool into actix-web application state

--- crates/voxora-api/src/main.rs
+++ crates/voxora-api/src/main.rs
@@ -3,16 +3,27 @@ use actix_web::{HttpServer, App, web};
 mod controllers;
 mod extractors;
 mod middleware;
-
+use voxora_db::create_pool_connection;
 use controllers::{signup, login};
+
+
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
+    let connections = match create_pool_connection().await {
+        Ok(pool) => pool,
+        Err(_) => {
+            eprintln!("faiied to create connection to the database");
+            std::process::exit(1);
+        },
+    };
+    let pgpool  = web::Data::new(connections);
     println!("voxora-api is starting on port 8080");
 
     HttpServer::new(move || {
         //expects the factory to return an application instance 
         App::new()
+            .app_data(pgpool.clone())
             .service(signup)
             .service(login)
     })
