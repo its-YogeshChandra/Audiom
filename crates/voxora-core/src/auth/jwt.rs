dc56d32988f6b4737658700c30999ad96dcc25a1
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-22 18:13:50 +0530

    feat: implement JWT token creation and verification utilities in voxora-core

--- crates/voxora-core/src/auth/jwt.rs
+++ crates/voxora-core/src/auth/jwt.rs
@@ -1,6 +1,10 @@
-use jsonwebtoken::{encode,  Header, EncodingKey, DecodingKey, Validation};
+use jsonwebtoken::{encode,decode, Header, EncodingKey, DecodingKey, Validation, Algorithm};
+use jsonwebtoken::errors::ErrorKind;
 use serde::{Deserialize, Serialize};
-use db_services::get_user_data;
+use uuid::Uuid;
+use dotenvy::dotenv;
+use std::env;
+
 
 //read the secret from the env file 
 
@@ -12,20 +16,54 @@ use db_services::get_user_data;
 
 
 //create the jwt token
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Claims{
     id: Uuid,
     email: String,
     name: String,
 }
 
-fn create_token (){
-    //get the values from the db
-    let db_result = get_user_data();
-    
+pub fn create_token (data: Claims) -> String {
+dotenv().ok();
 
-    //
+let jwt_secret_key = env::var("JWT_SECRET").expect("JWT_SECRET not found");
+let encoding_key = EncodingKey::from_secret(jwt_secret_key.as_bytes());
+
+//create the header
+let header = Header::default();
+
+//encode the given data 
+let token = match encode(&header, &data, &encoding_key){
+    Ok(token) => token,
+    Err(error) => {
+        eprintln!("Error creating token: {}", error);
+        return error.to_string()
+    },
+};
+
+//for check we can decode and check if we get the same values or not 
+//but that's future me problem 
+ token
 }
 
 
 
-//verify the jwt token 
+//verify the jwt token
+pub fn verify_token(token: &str) -> Result<Claims, String> {
+    dotenv().ok();
+
+    let jwt_secret_key = env::var("JWT_SECRET").expect("JWT_SECRET not found");
+    let decoding_key = DecodingKey::from_secret(jwt_secret_key.as_bytes());
+
+    //decode the given data 
+    let decoded_result = match decode::<Claims>(token, &decoding_key, &Validation::new(Algorithm::HS256)){
+        Ok(token) => token,
+        Err(error) => match error.kind() {
+            ErrorKind::InvalidToken => panic!("Invalid token"),
+            _ => panic!("Invalid token")
+        }
+    };
+
+    Ok(decoded_result.claims)
+     
+} 
--- crates/voxora-core/src/lib.rs
+++ crates/voxora-core/src/lib.rs
@@ -1,2 +1,4 @@
 mod auth;
 mod models;
+
+//
