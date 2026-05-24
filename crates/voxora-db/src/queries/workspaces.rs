8b592c23ad305aada893eb1193f0072088239ffa
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-25 00:41:22 +0530

    feat: refactor add_members to accept user_credentials and initiate workspace_members insertion

--- crates/voxora-db/src/queries/workspaces.rs
+++ crates/voxora-db/src/queries/workspaces.rs
@@ -146,13 +146,17 @@ async fn delete_workspace(pool: PgPool, workspace_id: Uuid) -> Result<(), sqlx::
 }
 
 
-async fn add_members (pool : PgPool , workspace_id: Uuid , role: WorkspaceRole , members : Uuid){
+async fn add_members (pool : PgPool , workspace_id: Uuid , role: WorkspaceRole , user_credentials: String){
     //same the user must be  the owner of the workspace to do that 
     //add single member at a time (will add multiple members adding)
+    
+    let db_result = sqlx::query!(WorkspaceMembers =>"
+    INSERT INTO workspace_members
+    (
+    workspace_id,
+    " ,
 
-    //questions (how we add the member)
-    // on the basis of the name ( can be done (what if two people have same name ))
-    //on the basis of the username ( this is better ( username can be set it to unique while creating user ))
-    //create a whole new field for user name : member id ( any time to add memeber that id will be shared)
+)
 
+    
 }
