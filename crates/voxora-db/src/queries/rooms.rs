67c2d49e43399c63626e69f00890ce14e3efe752
its-YogeshChandra <pandittheroyal@gmail.com>
2026-05-29 01:29:52 +0530

    feat: implement room database operations for querying, updating, and deleting records

--- crates/voxora-db/src/queries/rooms.rs
+++ crates/voxora-db/src/queries/rooms.rs
@@ -32,6 +32,9 @@ pub struct UpdateRoom{
     pub max_peers: Option<i32>,
 }
 
+//has to know about fetch one , fetch optional , fetch all, execute (in sqlx::query as!)
+
+
 async fn create_room(pool: &PgPool, new_room: NewRoom) -> Result<Room, sqlx::Error> {
     let room = sqlx::query_as!(
         Room,
@@ -65,8 +68,68 @@ async fn get_room_by_id(pool: &PgPool, room_id: Uuid) -> Result<Room, sqlx::Erro
     }
 }
 
-async fn get_rooms_by_project_id(){}
+async fn get_rooms_by_project_id(pool : &PgPool , project_id : Uuid) -> Result<Vec<Room>, sqlx::Error>{
+    
+    let rooms = sqlx::query_as!(
+        Room,
+        r#"
+        SELECT * FROM rooms WHERE project_id = $1
+        "#,
+        project_id
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rooms)
+}
 
-async fn update_room(){}
+async fn update_room(pool : &PgPool , room_id : Uuid , update_room : UpdateRoom) -> Result<Room, sqlx::Error>{
+    let room = sqlx::query_as!(
+        Room,
+        r#"
+        UPDATE rooms 
+        SET name = COALESCE($1, name),
+            slug = COALESCE($2, slug),
+            host_id = COALESCE($3, host_id),
+            max_peers = COALESCE($4, max_peers)
+        WHERE id = $5
+        RETURNING id, project_id, name, slug, host_id, status, max_peers, created_at, ended_at
+        "#,
+        update_room.name,
+        update_room.slug,
+        update_room.host_id,
+        update_room.max_peers,
+        room_id
+    )
+    .fetch_one(pool)
+    .await?;
+    Ok(room)
+}
 
-async fn delete_room(){}
+async fn update_room_status(pool : &PgPool , room_id : Uuid , status : String) -> Result<Room , sqlx::Error>{
+    let room = sqlx::query_as!(
+        Room,
+        r#"
+        UPDATE rooms 
+        SET status = $1
+        WHERE id = $2
+        RETURNING id, project_id, name, slug, host_id, status, max_peers, created_at, ended_at
+        "#,
+        status,
+        room_id
+    )
+    .fetch_one(pool)
+    .await?;
+    Ok(room)
+}
+
+async fn delete_room(pool : &PgPool , room_id : Uuid) -> Result<u64 , sqlx::Error>{
+    let result = sqlx::query!(
+        r#"
+        DELETE FROM rooms WHERE id = $1
+        "#,
+        room_id
+    )
+    .execute(pool)
+    .await?;
+    Ok(result.rows_affected())
+}
